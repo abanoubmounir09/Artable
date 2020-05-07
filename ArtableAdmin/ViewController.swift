fe6b1f9e9e106d3daa83bd6e9ede782c9d2cf14d
pop <pop@pops-Mac.local>
2020-05-07 00:50:43 -0700

    initial commit

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by pop on 5/7/20.
+//  Copyright © 2020 pop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
